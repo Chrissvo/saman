10e5c39bbe7c92983896c80ceede024cbd1a038e
Chrissvo <git@christian.surf>
2015-09-19 18:56:19 +0200

    Use currentPath to display the correct breadcrumb

--- app/components/bread-crumbs/component.js
+++ app/components/bread-crumbs/component.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  currentPath: function() {
+    return this.container.lookup('controller:application').get('currentPath');
+  }.property(),
+
+  breadCrumb: function() {
+    const currentPath = this.get('currentPath');
+    let breadCrumb;
+    switch (currentPath) {
+      case 'index':
+        breadCrumb = '';
+        break;
+      case 'business.model':
+        breadCrumb = 'Zakelijk Rekenmodel';
+    }
+    return breadCrumb;
+  }.property()
+
 });
