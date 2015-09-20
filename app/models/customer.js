8866ceea21b7d6551b3ddd80edb7a0933ffa704f
Chrissvo <git@christian.surf>
2015-09-20 16:46:40 +0200

    Add customer model

---
+++ app/models/customer.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  
+  name: DS.attr('string'),
+  address: DS.attr('string'),
+  zip: DS.attr('string'),
+  city: DS.attr('string'),
+  email: DS.attr('string'),
+  phone: DS.attr('string')
+
+});
