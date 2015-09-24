4ce803f03f2d131e1619e7bcdd463857cda07c06
Chrissvo <git@christian.surf>
2015-09-24 17:30:57 +0200

    Add system model

---
+++ app/models/system.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+
+  panelAmount: DS.attr('number'),
+  panelPower: DS.attr('number'),
+  panelPrice: DS.attr('number'),
+
+  roofOrientation: DS.attr('string'),
+  roofSlope: DS.attr('number'),
+
+  sdePhase: DS.attr('string'),
+  apxPrice: DS.attr('number'),
+
+  customer: DS.belongsTo('customer')
+
+});
