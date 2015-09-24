5c9bff73f96258ffb9437e21bbd0dd7fedc426ed
Chrissvo <git@christian.surf>
2015-09-24 16:48:56 +0200

    Set a customerId when executing saveCustomer

--- app/business/model/controller.js
+++ app/business/model/controller.js
@@ -4,21 +4,27 @@ export default Ember.Controller.extend({
 
   customerForm: true,
   companyForm: false,
+  customerId: undefined,
   applicationController: Ember.inject.controller('application'),
 
   actions: {
 
     saveCustomer: function(data) {
       const newCustomer = this.store.createRecord('customer', data);
-      newCustomer.save().then(function() {
+      newCustomer.save().then(function(customer) {
         // success
+        this.set('customerId', customer.get('id'));
         this.set('customerForm', false);
         return this.set('companyForm', true);
       }.bind(this)).catch(function(error) {
         // fail
         return this.get('applicationController').notify({
           id: 'fail.saveCustomer',
-          message: 'Opslaan is mislukt! ' + error,
+          message: 'Opslaan van deze klant is mislukt! ' + error,
+          type: 'error'
+        });
+      }.bind(this));
+    },
           type: 'error'
         });
       }.bind(this));
