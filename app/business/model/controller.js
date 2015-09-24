6dbf1b3b754d95e3f4a94177a6ebdd9682574b9e
Chrissvo <git@christian.surf>
2015-09-24 12:25:57 +0200

    Finish saveCustomer function

--- app/business/model/controller.js
+++ app/business/model/controller.js
@@ -3,13 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   customerForm: true,
+  companyForm: false,
+  applicationController: Ember.inject.controller('application'),
 
   actions: {
 
     saveCustomer: function(data) {
-      this.store.createRecord('customer', data);
+      const newCustomer = this.store.createRecord('customer', data);
+      newCustomer.save().then(function() {
+        // success
+        this.set('customerForm', false);
+        return this.set('companyForm', true);
+      }.bind(this)).catch(function(error) {
+        // fail
+        return this.get('applicationController').notify({
+          id: 'fail.saveCustomer',
+          message: 'Opslaan is mislukt! ' + error,
+          type: 'error'
+        });
+      }.bind(this));
     }
-
   }
 
 });
