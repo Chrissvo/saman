608f2c90a2371c88ded5d2b4e77fa228f7135901
Chrissvo <git@christian.surf>
2015-09-24 16:43:44 +0200

    Add companies to the customer model

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,12 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+
   name: DS.attr('string'),
   address: DS.attr('string'),
   zip: DS.attr('string'),
   city: DS.attr('string'),
   email: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  companies: DS.hasMany('company')
 
 });
