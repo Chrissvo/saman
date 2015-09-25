ca65abe68cf11f6d1a1587772b4ec642b8615f3f
Chrissvo <git@christian.surf>
2015-09-25 20:32:41 +0200

    Write situationData function

--- app/business/model/results/controller.js
+++ app/business/model/results/controller.js
@@ -2,6 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  queryParams: ['systemId']
+  queryParams: ['systemId'],
+
+  situationData: function() {
+    return [{
+      label: 'Juridische bedrijfsvorm',
+      value: this.get('model.company.companyType')
+    }, {
+      label: 'Inkomen / winst',
+      value: this.get('model.company.incomeCategory')
+    }, {
+      label: 'Huidig verbruik',
+      value: this.get('model.company.energyUsage').toFixed(0) + ' kWh/jaar'
+    }, {
+      label: 'Netto energieprijs',
+      value: '€ ' + this.get('model.company.energyPrice').toFixed(3) + ' per kWh'
+    }, {
+      label: 'Factor eigen verbruik',
+      value: this.get('model.company.factorOwnUsage').toFixed(0) + '%'
+    }, {
+      label: 'Aansluiting',
+      value: this.get('model.company.connection')
+    }];
+  }.property(),
 
 });
