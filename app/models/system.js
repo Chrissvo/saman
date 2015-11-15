693af6862b1a8d2b4d280866537a08084284bdb7
Chrissvo <git@christian.surf>
2015-11-15 22:36:15 +0100

    Add defaultValues and remove those from the html

--- app/models/system.js
+++ app/models/system.js
@@ -3,14 +3,20 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
   panelAmount: DS.attr('number'),
-  panelPower: DS.attr('number'),
-  panelPrice: DS.attr('number'),
+  panelPower: DS.attr('number', {
+    defaultValue: 265
+  }),
+  panelPrice: DS.attr('number', {
+    defaultValue: 0.95
+  }),
 
   roofOrientation: DS.attr('string'),
   roofSlope: DS.attr('number'),
 
   sdePhase: DS.attr('string'),
-  apxPrice: DS.attr('number'),
+  apxPrice: DS.attr('number', {
+    defaultValue: 0.045
+  }),
 
   customer: DS.belongsTo('customer'),
   company: DS.belongsTo('company')
